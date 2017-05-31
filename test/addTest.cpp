c7af3d395dbebba61e738df173d8f48e971e0624
Heewon Chung <runghyun@snu.ac.kr>
2017-05-31 17:44:08 +0900

    update parameter

--- test/addTest.cpp
+++ test/addTest.cpp
@@ -21,7 +21,7 @@ int main(){
     long p = 2;
     long r = 1;
     long security = 80;
-    long L = 6;
+    long L = 7;
     long bitSize = 6;
     long c = 3;
     long m = FindM(security, L, c, p, 0, 0, 0);
