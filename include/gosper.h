aa4ca434b1c243f3908d27641065721878bff687
Heewon Chung <runghyun@snu.ac.kr>
2017-06-27 18:26:15 +0900

    update Gosper algorithm

---
+++ include/gosper.h
@@ -0,0 +1,26 @@
+#ifndef GOSPER_H
+#define GOSPER_H
+
+#include <cstdlib>
+#include <vector>
+#include <cassert>
+
+#include "EncryptedArray.h"
+#include "Ctxt.h"
+
+#include "comparison.h"
+
+// z = (numConst + numCoeff * x) / (denConst + denCoeff * x)
+typedef struct cfObject{
+    Ctxt numConst;
+    Ctxt numCoeff;
+    Ctxt denConst;
+    Ctxt denCoeff;
+} CFObject;
+
+inline void intake(cfObject&, const Ctxt&);
+inline void intakeInfty(cfObject&);
+inline void outtake(Ctxt&, const cfObject&, const long, const EncryptedArray&);
+void gosper(vector<Ctxt>&, const vector<Ctxt>&, cfObject&, const long, const EncryptedArray&);
+
+#endif
---
+++ src/gosper.cpp
@@ -0,0 +1,46 @@
+#include "gosper.h"
+
+using namespace std;
+using namespace NTL;
+
+inline void intake(cfObject& object, const Ctxt& pq){
+    Ctxt temp1 = object.numConst;
+    Ctxt temp2 = object.denConst;
+
+    object.numConst = object.numCoeff;
+    object.denConst = object.denCoeff;
+
+    object.numCoeff.multiplyBy(pq);
+    object.denCoeff.multiplyBy(pq);
+
+    object.numCoeff.addCtxt(temp1);
+    object.denCoeff.addCtxt(temp2);
+}
+
+inline void intakeInfty(cfObject& object){
+    object.numConst = object.numCoeff;
+    object.denConst = object.denCoeff;
+}
+
+inline void outtake(Ctxt& rem, const cfObject& object, const long numLength, const EncryptedArray& ea){
+    const FHEPubKey& publicKey = object.numCoeff.getPubKey();
+    bool lessThan = 1;
+    rem = object.numCoeff;
+
+    for(unsigned long i = 0; i < numLength; i++){
+        Ctxt isLess(publicKey);
+        comparisonTestOverZ(isLess, rem, object.denCoeff, lessThan, numLength, ea);
+        isLess.multiplyBy(object.denCoeff);
+        rem -= isLess;
+    }
+}
+
+void gosper(vector<Ctxt>& outputPQ, const vector<Ctxt>& inputPQ, cfObject& object, const long numLength, const EncryptedArray& ea){
+    for(unsigned long i = 0; i < inputPQ.size(); i++){
+        intake(object, inputPQ[i]);
+    }
+
+    for(unsigned long i = 0; i < numLength; i++){
+        outtake(outputPQ[i], object, numLength, ea);
+    }
+}
